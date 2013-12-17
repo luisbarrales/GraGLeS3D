b9128e811a3bffb01e6e158ae75cedc26a1529cd
Nikola Velinov <nikola.velinov@rwth-aachen.de>
2013-12-17 15:17:06 +0100

    Adding class that handles dimensional buffer.

---
+++ dimensionalBuffer.h
@@ -0,0 +1,160 @@
+#ifndef		__DIMENSIONAL_BUFFER__
+#define		__DIMENSIONAL_BUFFER__
+
+#include <vector>
+
+/*!
+ * \class DimensionalBuffer
+ * \brief Class that encapsulates a dimensional buffer
+ *
+ * Dimensional buffer for ease in management of dimensional data. The data exists in space, where
+ * the minimum x and y coordinates are 0 and the maximal coordinate value is bounded by the size
+ * of signed 32 bit integer.
+ */
+template<class T>
+class	DimensionalBuffer
+{
+public:
+	/*!
+	 * \brief Basic constructor. Just initializes the object with some default values.
+	 */
+	DimensionalBuffer::DimensionalBuffer() :
+			m_xMin(0), m_xMax(1), m_yMin(0), m_yMax(1)
+	{}
+	/*!
+	* \brief Constructor, which receives the coordinates of the managed region.
+	*
+	* This constructor takes the boundaries of the managed region as input, and properly
+	* initializes the internal memory.
+    */
+	DimensionalBuffer(unsigned int upperLeftX, unsigned int upperLeftY,
+					  unsigned int lowerRightX, unsigned int lowerRightY) :
+		m_xMin(upperLeftX), m_yMin(upperLeftY), m_xMax(lowerRightX),
+		m_yMax(lowerRightY)
+	{
+		m_values.resize((m_xMax - m_xMin + 1) * (m_yMax - m_yMin + 1));
+	}
+	/*!
+	* \brief Default destructor.
+    */
+	~DimensionalBuffer()
+	{}
+	/*!
+	* \brief Method that returns the value at the given coordinates.
+	*
+	* This method retreives the value at the specified coordinates. If such a value does not exist
+	* this method throws an out of bound exception.
+	* \param x the x coordinate of the element.
+	* \param y the y coordinate of the element.
+	*/
+	T getValueAt(unsigned int x, unsigned int y) const
+	{
+		//Will throw exception if accessed out of bound.
+		//TODO: Analyze performance and replace with [] if needed.
+		return m_values.at((y - m_yMin) * (m_xMax - m_xMin + 1) + (x - m_xMin));
+	}
+	/*!
+	* \brief Method that sets the value at the given coordinates.
+	*
+	* This method sets the value at the specified coordinates. If such a value does not exist
+	* this method throws an out of bound exception.
+	* \param x the x coordinate of the element.
+	* \param y the y coordinate of the element.
+	* \param value the value to be set.
+	*/
+	void setValueAt(unsigned int x, unsigned int y, T value)
+	{
+		//Will throw exception if accessed out of bound.
+		//TODO: Analyze performance and replace with [] if needed.
+		m_values.at((y - m_yMin) * (m_xMax - m_xMin + 1) + (x - m_xMin)) = value;
+	}
+	/*!
+	* \brief This method resizes the dimensions.
+	*
+	* This method resizes the dimensions and properly manages the internal data.
+	* \param upperLeftX the desired new minimal x coordinate.
+	* \param upperLeftY the desired new minimal y coordinate.
+	* \param lowerRightX the desired new maximal x coordinate.
+	* \param lowerRightY the desired new maximal y coordinate.
+	*/
+	void resize(unsigned int upperLeftX, unsigned int upperLeftY,
+			  	unsigned int lowerRightX, unsigned int lowerRightY)
+	{
+		m_xMin = upperLeftX;
+		m_xMax = lowerRightX;
+		m_yMin = upperLeftY;
+		m_yMax = lowerRightY;
+
+		m_values.resize((m_xMax - m_xMin + 1) * (m_yMax - m_yMin + 1));
+	}
+
+	/*!
+	* \brief This method resizes the dimensions.
+	*
+	* Resizes the current area to a square area and manages the internal memory.
+	* \param maximumLength The maximal value for the x or y coordinates.
+	*/
+	void resizeToSquare(unsigned int maximumLength)
+	{
+		int grid_size = maximumLength;
+		unsigned int height = m_yMax - m_yMin;
+		unsigned int width  = m_xMax - m_xMin;
+		//First resize the rectangle to a square
+		if(width == height)
+			return;
+		else if (width > height)
+		{
+			int diff = width - height;
+			m_yMin -= diff/2;
+			m_yMax += diff/2 + diff%2;
+		}
+		else
+		{
+			int diff = height - width;
+			m_xMin -= diff/2;
+			m_xMax += diff/2 + diff%2;
+		}
+		//Now move the square in the bounds if it has left them
+		if( m_xMin < 0 )
+		{
+			int delta = -(int)m_xMin;
+			m_xMax += delta;
+			m_xMin += delta;
+		}
+		else if (m_xMax > grid_size)
+		{
+			int delta = grid_size - m_xMax;
+			m_xMax += delta;
+			m_xMin += delta;
+		}
+		if( m_yMin < 0 )
+		{
+			int delta = -(int)m_yMin;
+			m_yMax += delta;
+			m_yMin += delta;
+		}
+		else if (m_yMax > grid_size)
+		{
+			int delta = grid_size - m_yMax;
+			m_yMax += delta;
+			m_yMin += delta;
+		}
+
+		m_values.resize((m_xMax-m_xMin+1) * (m_yMax-m_yMin+1));
+	}
+	/*!
+	* \brief This method fills the area with the provided values.
+	*/
+	void clearValues(T value)
+	{
+		std::fill(m_values.begin(), m_values.end(), value);
+	}
+private:
+
+	int 	m_xMin;
+	int 	m_xMax;
+	int 	m_yMin;
+	int 	m_yMax;
+	std::vector<T>	m_values;
+};
+#endif		//__DISTANCE_BUFFER__
