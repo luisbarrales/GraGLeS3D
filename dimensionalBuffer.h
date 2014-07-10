9d6f9454c8ed3c4d039285e2ab3d8b58fbe54978
Christian Mießen lx13 <miessen@imm.rwth-aachen.de>
2014-07-10 12:21:27 +0200

    dimensionalbuffer.h changed to 3D buffer- done not tested

--- dimensionalBuffer.h
+++ dimensionalBuffer.h
@@ -11,7 +11,7 @@
  * \brief Class that encapsulates a dimensional buffer
  *
  * Dimensional buffer for ease in management of dimensional data. The data exists in space, where
- * the minimum x and y coordinates are 0 and the maximal coordinate value is bounded by the size
+ * the minimum x and y and z (new) coordinates are 0 and the maximal coordinate value is bounded by the size
  * of signed 32 bit integer.
  */
 template<class T>
@@ -22,7 +22,7 @@ public:
 	 * \brief Basic constructor. Just initializes the object with some default values.
 	 */
 	DimensionalBuffer() :
-			m_xMin(0), m_xMax(1), m_yMin(0), m_yMax(1)
+			m_xMin(0), m_xMax(1), m_yMin(0), m_yMax(1), m_zMin(0), m_zMax(1)
 	{}
 	/*!
 	* \brief Constructor, which receives the coordinates of the managed region.
@@ -31,11 +31,11 @@ public:
 	* initializes the internal memory.
     */
 	DimensionalBuffer(unsigned int upperLeftX, unsigned int upperLeftY,
-					  unsigned int lowerRightX, unsigned int lowerRightY) :
+					  unsigned int lowerRightX, unsigned int lowerRightY, unsigned int frontEnd, unsigned int backEnd) :
 		m_xMin(upperLeftX), m_yMin(upperLeftY), m_xMax(lowerRightX),
-		m_yMax(lowerRightY)
+		m_yMax(lowerRightY), m_zMin(frondEnd), m_zMax(backEnd)
 	{
-		resize(m_xMin, m_yMin, m_xMax, m_yMax);
+		resize(m_xMin, m_yMin, m_xMax, m_yMax, m_zMin, m_zMax);
 	}
 	/*!
 	* \brief Default destructor.
@@ -50,11 +50,12 @@ public:
 	* \param row the y coordinate of the element.
 	* \param column the x coordinate of the element.
 	*/
-	T& getValueAt(unsigned int row, unsigned int column)
+	T& getValueAt(unsigned int row, unsigned int column, unsigned int layer)
 	{
 		//Will throw exception if accessed out of bound.
 		//TODO: Analyze performance and replace with [] if needed.
-		return m_values.at((row - m_yMin) * (m_xMax - m_xMin) + (column - m_xMin));
+		//TODO the idea is to build a cubic matric layer by layer -- correct???
+		return m_values.at(layer *(row - m_yMin) * (m_xMax - m_xMin) + (column - m_xMin));
 	}
 	/*!
 	* \brief Method that sets the value at the given coordinates.
@@ -65,20 +66,20 @@ public:
 	* \param column the x coordinate of the element.
 	* \param value the value to be set.
 	*/
-	void setValueAt(unsigned int row, unsigned int column, T value)
+	void setValueAt(unsigned int row, unsigned int column, unsigned int layer, T value)
 	{
 		//Will throw exception if accessed out of bound.
 		//TODO: Analyze performance and replace with [] if needed.
-		m_values.at((row - m_yMin) * (m_xMax - m_xMin) + (column - m_xMin)) = value;
+		m_values.at(layer * (row - m_yMin) * (m_xMax - m_xMin) + (column - m_xMin)) = value;
 	}
 	/*\brief Method that checks whether a point lies within the dimensional buffer.
 	 *
 	 * \param row the y coordinate of the point to test.
 	 * \param column the x coordinate of the point to test.
 	 */
-	bool isPointInside(unsigned int row, unsigned int column)
+	bool isPointInside(unsigned int row, unsigned int column, unsigned int layer)
 	{
-		return row >= m_yMin && row < m_yMax && column >= m_xMin && column < m_xMax;
+		return row >= m_yMin && row < m_yMax && column >= m_xMin && column < m_xMax && layer >= m_zMin && layer < m_zMax;
 	}
 	/*!
 	* \brief This method resizes the dimensions.
@@ -90,14 +91,16 @@ public:
 	* \param lowerRightY the desired new maximal y coordinate.
 	*/
 	void resize(unsigned int upperLeftX, unsigned int upperLeftY,
-			  	unsigned int lowerRightX, unsigned int lowerRightY)
+			  	unsigned int lowerRightX, unsigned int lowerRightY, unsigned int frontEnd, unsigned int backEnd )
 	{
 		m_xMin = upperLeftX;
 		m_xMax = lowerRightX;
 		m_yMin = upperLeftY;
 		m_yMax = lowerRightY;
+		m_zMin = frontEnd;
+		m_zMax = backEnd;
 
-		m_values.resize((m_xMax - m_xMin) * (m_yMax - m_yMin));
+		m_values.resize((m_zMax - m_zMin)*(m_xMax - m_xMin) * (m_yMax - m_yMin));
 	}
 
 	/*!
@@ -106,26 +109,44 @@ public:
 	* Resizes the current area to a square area and manages the internal memory.
 	* \param maximumLength The maximal value for the x or y coordinates.
 	*/
-	void resizeToSquare(unsigned int maximumLength)
+	void resizeToCube(unsigned int maximumLength)
 	{
 		int grid_size = maximumLength-1;
 		unsigned int height = m_yMax - m_yMin;
 		unsigned int width  = m_xMax - m_xMin;
+		unsigned int depth  = m_zMax - m_zMin;
 		//First resize the rectangle to a square
-		if(width == height)
+		if(width == height && width == depth)
 			return;
-		else if (width > height)
-		{
-			int diff = width - height;
-			m_yMin -= diff/2;
-			m_yMax += diff/2 + diff%2;
+
+		else{
+			//		find largest dimension:
+			if (width > height)	ref = width;
+			else ref = height;
+			if(ref < depth) ref =depth;
+
+
+			if (ref > width)
+			{
+				int diff = ref - width;
+				m_xMin -= diff/2;
+				m_xMax += diff/2 + diff%2;
+			}
+			if (ref > height)
+			{
+				int diff = ref - height;
+				m_yMin -= diff/2;
+				m_yMax += diff/2 + diff%2;
+			}
+			if (ref > depth)
+			{
+				int diff = ref - depth;
+				m_zMin -= diff/2;
+				m_zMax += diff/2 + diff%2;
+			}
 		}
-		else
-		{
-			int diff = height - width;
-			m_xMin -= diff/2;
-			m_xMax += diff/2 + diff%2;
-		}
+
+
 		//Now move the square in the bounds if it has left them
 		if( m_xMin < 0 )
 		{
@@ -151,9 +172,24 @@ public:
 			m_yMax += delta;
 			m_yMin += delta;
 		}
+		if( m_zMin < 0 )
+		{
+			int delta = -(int)m_zMin;
+			m_zMax += delta;
+			m_zMin += delta;
+		}
+		else if (m_zMax > grid_size)
+		{
+			int delta = grid_size - m_zMax;
+			m_zMax += delta;
+			m_zMin += delta;
+		}
 
-		resize(m_xMin, m_yMin, m_xMax, m_yMax);
+
+		resize(m_xMin, m_yMin, m_xMax, m_yMax, m_zMin, m_zMax);
 	}
+
+
 	/*!
 	* \brief This method fills the area with the provided values.
 	*/
@@ -170,6 +206,10 @@ public:
 	{return m_yMin;}
 	inline int getMaxY() const
 	{return m_yMax;}
+	inline int getMinZ() const
+	{return m_zMin;}
+	inline int getMaxZ() const
+	{return m_zMax;}
 	inline T* getRawData()
 	{return &m_values[0];}
 
@@ -178,6 +218,8 @@ private:
 	int 	m_xMax;
 	int 	m_yMin;
 	int 	m_yMax;
+	int 	m_zMIn;
+	int 	m_zMax;
 protected:
 	ExpandingVector<T>	m_values;
 };
